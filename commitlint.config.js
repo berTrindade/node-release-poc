51cea7a652c1e8ece98b03d3c9fcb55e903b352c
Bernardo T. Abreu <bernardo.trindade-de-abreu@ustwo.com>
2025-10-31 10:18:21 +0000

    fix: disable body-max-line-length rule for release commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Disable body-max-line-length to allow generated release commits with URLs
+    // This allows standard-version and semantic-release commits to pass validation
+    'body-max-line-length': [0, 'always', Infinity],
+  },
 };
