c4a19c8f4a15b0ac5166615e8a1e5a4d8f7c328a
Bernardo T. Abreu <bernardo.trindade-de-abreu@ustwo.com>
2025-10-31 09:59:23 +0000

    feat: add /info endpoint with project information

--- src/app.js
+++ src/app.js
@@ -10,4 +10,12 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
+app.get('/info', (req, res) => {
+  res.json({ 
+    name: 'node-release-poc',
+    description: 'POC for release workflows',
+    workflows: ['manual-release', 'auto-release']
+  });
+});
+
 module.exports = app;
